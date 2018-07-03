39b7683c1275296cc96a4fed742772feb5c6a903
junzhezhang <zhangjunzhetom@gmail.com>
2018-07-03 20:00:19 +0800

    print new Block from Tensor.cc

--- src/core/tensor/tensor.cc
+++ src/core/tensor/tensor.cc
@@ -135,8 +135,8 @@ void Tensor::AsType(const DataType type) {
   if (data_type_ != type) {
     if (block_ != nullptr && block_->DecRefCount() == 0)
       device_->FreeBlock(block_);
-    std::cout<<"Call NewBlock in Tensor "<<Product(shape) * SizeOf(type)<<std::endl;
-    block_ = device_->NewBlock((int)(Product(shape) * SizeOf(type)));
+    std::cout<<"Call NewBlock in Tensor "<<Product(shape_) * SizeOf(type)<<std::endl;
+    block_ = device_->NewBlock((int)(Product(shape_) * SizeOf(type)));
     data_type_ = type;
   }
 }
