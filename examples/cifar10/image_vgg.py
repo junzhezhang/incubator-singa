bb6fabc4f967ddf1bf03367a8d9708998bb1f503
junzhezhang <zhangjunzhetom@gmail.com>
2018-07-03 18:34:59 +0800

    init_param exceptions

--- examples/cifar10/image_vgg.py
+++ examples/cifar10/image_vgg.py
@@ -50,6 +50,7 @@ def init_params(net, weight_path=None):
             elif 'var' in pname or 'gamma' in pname:
                 pval.set_value(1)
             else:
+              initializer.gaussian(pval, 0, pval.shape[1])
               print ("not in any condition, DSB!!!")
     else:
         net.load(weight_path, use_pickle='pickle' in weight_path)
